683e81074cb303540be1a4723e3efdeed51a6db0
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-03-03 09:00:59 +0000

    Apple Orange Done

--- apple-and-orange.php
+++ apple-and-orange.php
@@ -12,9 +12,25 @@
  *  6. INTEGER_ARRAY oranges
  */
 
-function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges) {
-    // Write your code here
+function countApplesAndOranges($s, $t, $a, $b, $apples, $oranges)
+{
+    $totalApples = 0;
+    $totalOranges = 0;
 
+    foreach ($apples as $apple) {
+        $apple += $a;
+        if ($apple >= $s && $apple <= $t) {
+            $totalApples++;
+        }
+    }
+    foreach ($oranges as $orange) {
+        $orange += $b;
+        if ($orange >= $s && $orange <= $t) {
+            $totalOranges++;
+        }
+    }
+
+    print $totalApples."\n".$totalOranges."\n";
 }
 
-echo countApplesAndOranges();
+echo countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
