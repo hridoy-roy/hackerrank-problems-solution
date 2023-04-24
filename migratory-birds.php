0254ced6946f2f1482ceb5a4425e6f17cd9b7c01
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-04-24 15:23:48 +0000

    empty  file

---
+++ migratory-birds.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds($arr)
+{
+}
